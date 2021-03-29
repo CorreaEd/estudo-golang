a5f346278a72b89e36e99abdf357bb5292b5b1ad
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-03-29 12:31:13 -0300

    add exercício variáveis
    Foram adicionados os programas var_idade.go, var_simples.go, var_reflect.go e var_golang.go

--- var_simples.go
+++ var_simples.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Douglas"
-	var idade int = 24
+	var nome = "Douglas"
+	var idade = 24
 	var versão float32 = 1.1
 	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versão)
