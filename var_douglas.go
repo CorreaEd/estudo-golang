3b5388859b0daca67210650cfcb5f6ca6d30564e
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-03-29 01:05:34 -0300

    add exercício variáveis
    Foi adicionado o programa var_douglas.go

---
+++ var_douglas.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "douglas"
+	var versão float32 = 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versão)
+}
