0b6a5c43b72dec36f1719fd36d92f54add14889b
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-03-29 01:28:16 -0300

    add exercício variáveis
    Foi corrigido erro de digitação no programa var_douglas.go

--- var_idade.go
+++ var_idade.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Douglas"
+	var nome string = "douglas"
+	var idade int = 24
 	var versão float32 = 1.1
-	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versão)
 }
