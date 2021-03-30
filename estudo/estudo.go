7ebf462a57a7ca49b98cb35772c8849bcdd4d181
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-03-30 15:40:06 -0300

    add função time.Sleep
    Foi adicionado o package time; A função time.Sleep; Criamos constantes de "monitoramento" e "delay"; E também criamos func testaSites para uma melhor organização do código.

--- estudo/estudo.go
+++ estudo/estudo.go
@@ -4,26 +4,19 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"reflect"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 
-	var sites [4]string
-	sites[0] = "https://www.alura.com.br/"
-	sites[1] = "https://www.alura.com.br/dsadaga"
-	sites[2] = "https://random-status-code.herokuapp.com/"
-	sites[3] = "https://www.alura.com.br/xsafgsa"
-	/* 	sites[4] = "https://random.com/"
-	Como o array criado tem [4] posições, o que exceder as 4 posições,
-	por exemplo, será entendido como invalid array index */
+	exibirIntroducao()
 
-	fmt.Println(reflect.TypeOf(sites))
-	fmt.Println(sites)
-	exibeNomes()
-	/* exibirIntroducao() */
 	for {
-		/* 	exibirMenu() */
+
+		exibirMenu()
 
 		comando := lerComando()
 
@@ -62,15 +55,30 @@ func lerComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
 	return comandoLido
 }
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://www.alura.com.br/"
+	sites := []string{"https://www.alura.com.br/", "https://random-status-code.herokuapp.com/", "https://www.caelum.com.br"}
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSites(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+
+}
+
+func testaSites(site string) {
 	resp, _ := http.Get(site)
-	/* fmt.Println(resp) */
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site", site, "foi carregado com sucesso!")
@@ -79,46 +87,3 @@ func iniciarMonitoramento() {
 	}
 
 }
-
-func exibeNomes() {
-	nomes := []string{"Douglas", "Daniel", "Denis"}
-	fmt.Println(nomes)
-	fmt.Println(reflect.TypeOf(nomes))
-	fmt.Println("O meu slice tem", len(nomes), "itens")
-	fmt.Println("O meu slice tem capacidade para", cap(nomes), "itens")
-
-	nomes = append(nomes, "Eduardo", "Kelly")
-
-	fmt.Println(nomes)
-	fmt.Println(reflect.TypeOf(nomes))
-	fmt.Println("O meu slice tem", len(nomes), "itens")
-	fmt.Println("O meu slice tem capacidade para", cap(nomes), "itens")
-
-}
-
-/*Array X Slice - Na prática, não aparenta diferença alguma, mas têm suas diferenças:
-Array por exemplo, é limitado à quantidade de posições informada inicialmente
-
-	var exemplo [4]string
-
-Nesse caso, é um array de 4 posições:
-
-	exemplo[1] = "string1"
-	exemplo[2] = "string2"
-	exemplo[3] = "string3"
-	exemplo[4] = "string4"
-
-Em Go, os slices são arrays, são feitos em cima de um array,
-porém o slice facilita o seu uso, por se "ajustar" exatamente na quantidade de posições necessárias:
-
-	exemplo := []string{"string1", "string2", "string3", "string4", "string5", "string6"}
-
-
-Quando é feito um append >> o slice dobra a quantidade de posições iniciais!
-Ou seja, neste exemplo tinhamos 6 posições; Ao fazer um apend,
-ele passará a ter 12 posições, apesar de estar usando, neste exemplo, apenas 9 delas:
-
-	exemplo := []string{"string1", "string2", "string3", "string4", "string5", "string6"}
-	exemplo = append(exemplo, "string7", "string8", "string9")
-
-*/
