4bd2e2728337b8c27845f11a09a39a171ccf2fa7
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-03-29 22:26:09 -0300

    Foi add o menu com if else
    Exercícios Golang

--- estudo/estudo.go
+++ estudo/estudo.go
@@ -3,8 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Douglas"
-	var versão float32 = 1.1
+	nome := "Douglas"
+	versão := 1.1
 	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versão)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi", comando)
+
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa.")
+	} else {
+		fmt.Println("Não conheço este comando")
+	}
 }
