763a9c334de64aa87fca6efa9dd71ee60fc32b70
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-03-29 23:37:00 -0300

    Add função monitoramento

--- estudo/estudo.go
+++ estudo/estudo.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -14,7 +15,7 @@ func main() {
 
 	switch comando {
 	case 1:
-		fmt.Println("Monitorando...")
+		iniciarMonitoramento()
 	case 2:
 		fmt.Println("Exibindo Logs...")
 	case 0:
@@ -46,4 +47,18 @@ func lerComando() int {
 	fmt.Println("O comando escolhido foi", comandoLido)
 
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br/"
+	resp, _ := http.Get(site)
+	/* fmt.Println(resp) */
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site", site, "está com problemas. Status Code", resp.StatusCode)
+	}
+
 }
