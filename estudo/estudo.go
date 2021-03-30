7a20af605ea393a1cbb21519f75f4cdd9d77f797
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-03-29 22:27:17 -0300

    Foi atualizado if else > switch
    Exercícios Golang

--- estudo/estudo.go
+++ estudo/estudo.go
@@ -16,13 +16,26 @@ func main() {
 	fmt.Scan(&comando)
 	fmt.Println("O comando escolhido foi", comando)
 
-	if comando == 1 {
+	/*	if comando == 1 {
+			fmt.Println("Monitorando...")
+		} else if comando == 2 {
+			fmt.Println("Exibindo Logs...")
+		} else if comando == 0 {
+			fmt.Println("Saindo do programa.")
+		} else {
+			fmt.Println("Não conheço este comando")
+
+		}*/
+
+	switch comando {
+	case 1:
 		fmt.Println("Monitorando...")
-	} else if comando == 2 {
+	case 2:
 		fmt.Println("Exibindo Logs...")
-	} else if comando == 0 {
+	case 0:
 		fmt.Println("Saindo do programa.")
-	} else {
+	default:
 		fmt.Println("Não conheço este comando")
 	}
+
 }
