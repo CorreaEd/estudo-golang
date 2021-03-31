d0a20462422d26c850f9ff47676efb910391599c
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-03-30 23:33:30 -0300

    Add função imprimirLogs
    Foi adicionada a func imprimirLogs

--- estudo/estudo.go
+++ estudo/estudo.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -29,6 +30,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimirLogs()
 		case 0:
 			fmt.Println("Saindo do programa.")
 			os.Exit(0)
@@ -137,4 +139,16 @@ func registrarLog(site string, status bool) {
 	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + "-" + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
+}
+
+func imprimirLogs() {
+
+	arquivo, err := ioutil.ReadFile("logs.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
